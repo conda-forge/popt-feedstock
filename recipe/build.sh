93ce2d1a58cd857d5feb067675901f1b9bc7e2a0
UENO, M. <eunosium-1128@live.jp>
2025-04-04 10:03:09 +0000

    Enable to replace 'config.*' with gnuconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,12 @@
 
 set -e -o pipefail
 
+cp ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
 ./configure --prefix=${PREFIX} --disable-debug --disable-dependency-tracking
-
 make
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   make check
 fi
+
 make install
